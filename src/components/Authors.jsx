5193347d7a6039db1b2eabb1a937f6dd4bc648b3
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-02 13:44:02 +0530

    -completed exercise 8.8

---
+++ src/components/Authors.jsx
@@ -0,0 +1,30 @@
+const Authors = ({result}) => {
+    console.log()
+    const authors = result.data.allAuthors
+    console.log('in authors components',authors)
+    return(
+    <div>
+        <h1>Authors</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan="3">Authors</th>
+                </tr>
+            </thead>
+            <tbody>
+                {authors.map(author => <tr key={author.name}>
+                    <td>
+                        {author.name}
+                    </td>
+                    <td>
+                        {author.born}
+                    </td>
+                    <td>
+                        {author.bookCount}
+                    </td>
+                </tr>)}
+            </tbody>
+        </table>
+    </div>)
+}
+export default Authors
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-
+import { ApolloClient,InMemoryCache,ApolloProvider } from '@apollo/client'
+import { ALL_AUTHORS } from './queries.js'
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+// const query = ALL_AUTHORS
+// client.query({query}).then((response)=>console.log(response.data))
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 )
