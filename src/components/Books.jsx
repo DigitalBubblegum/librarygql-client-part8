518844459fe5d894011ad4457eda1187012d8127
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-06 17:39:50 +0530

    -completed exercise 8.19

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,14 +1,38 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
+
 const Books = () => {
+    const [booksFilter, setBooksFilter] = useState('all')
     const result = useQuery(ALL_BOOKS)
     if (result.loading) {
         return <div>loading...</div>
     }
-    console.log(result.data)
+    const genreList = []
+    // console.log(result.data)
     const books = result.data.allBooks
-    console.log(books.map(book=>book))
-    // console.log(books)
+    // console.log(books.map(book=>book.genres))
+    books.map(book => {
+        book.genres.forEach(element => {
+            if (genreList.includes(element)) {
+                null
+            }
+            else{
+                genreList.push(element)
+            }
+        })
+    })
+    // console.log(genreList)
+    const filterBooksList = () =>{
+        const reccoBooks = books.filter(book=>book.genres.includes(booksFilter))
+        return (
+            reccoBooks.map(book=><tr key={book.id}>
+                        <td>{book.title}</td>
+                        <td>{book.author.name}</td>
+                        <td>{book.published}</td>
+                    </tr>)
+        )
+    }
     return(
         <div>
             <h1>Books</h1>
@@ -19,13 +43,19 @@ const Books = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {books.map(book=><tr key={book.id}>
+                    {booksFilter === 'all'?
+                    books.map(book=><tr key={book.id}>
                         <td>{book.title}</td>
                         <td>{book.author.name}</td>
                         <td>{book.published}</td>
-                    </tr>)}
+                    </tr>):filterBooksList()
+                }
                 </tbody>
             </table>
+            <div>
+                <button onClick={()=>setBooksFilter('all')}>All</button>
+                {genreList.map(element => <button key = {element} onClick={()=>setBooksFilter(element)}>{element}</button>)}
+            </div>
         </div>
     )
 }
